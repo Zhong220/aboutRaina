6c80397cf1b4e780a171175285f4f43aad3b27a8
HuangRongRong <hrr20@HuangRongRongdeMacBook-Air.local>
2026-02-02 10:37:29 +0800

    feat(content)

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const resume = {
   },
   contact: {
     github: "https://github.com/Zhong220",
-    linkedin: "https://www.linkedin.com/",
+    linkedin: "https://www.linkedin.com/in/容-黃-a549713a9/",
     email: "hrrong422@example.com",
     resumePdfHref: `${import.meta.env.BASE_URL}resume.pdf`,
   },
